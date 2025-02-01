0888bc576e5628351e7a1ba2bfb9e1ec8a34e0d6
fexcode <2734664632@qq.com>
2025-02-01 17:22:23 +0800

    为create添加初始化内容的功能

--- cmd/create.go
+++ cmd/create.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
+	"time"
 )
 
 func createTable(tb_name string) error {
@@ -14,7 +15,18 @@ func createTable(tb_name string) error {
 		return errors.New("表已存在")
 	}
 
-	_, err := os.Create(TABLES_DIR + "/" + tb_name + ".toml")
+	file, err := os.Create(TABLES_DIR + "/" + tb_name + ".toml")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf(`
+[table-info]
+created_at = %s
+
+[records]
+	`, time.Now().Format("2006-01-02 15:04:05")))
 	if err != nil {
 		return err
 	}
