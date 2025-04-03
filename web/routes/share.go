6a8950e79276e5ab8609c7b40a06d542ec29fbb9
fexcode <2734664632@qq.com>
2025-04-03 19:02:22 +0800

    修复服务端get-info只能解析一个表格的BUG

--- web/routes/share.go
+++ web/routes/share.go
@@ -54,7 +54,7 @@ func str2ArrayTablesData(jsonData string) TablesData {
 	return data[0]
 }
 
-func str2ArrayTablesInfo(jsonData string) TablesData {
+func str2ArrayTablesInfo(jsonData string) []TablesData {
 	fmt.Println("解析json数据 ", jsonData)
 	var data []TablesData
 	err := json.Unmarshal([]byte(jsonData), &data)
@@ -62,5 +62,5 @@ func str2ArrayTablesInfo(jsonData string) TablesData {
 		log.Fatalf("Error parsing JSON: %v", err)
 	}
 	fmt.Println("解析到json数据 ", data)
-	return data[0]
+	return data
 }
